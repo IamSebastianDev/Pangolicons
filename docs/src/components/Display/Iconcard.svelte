4694be5a822a53b6aa02a441dab96fb859a8500f
Sebastian <sebastian@iamsebastian.dev>
2021-12-11 22:40:47 +0100

    Added Display section to the Website, which is used to display and handle all Icons

--- docs/src/components/Display/Iconcard.svelte
+++ docs/src/components/Display/Iconcard.svelte
@@ -1,10 +1,15 @@
 <script>
-	import Icon from '../UI/Icon.svelte';
+	import Pangol from '../UI/Pangol.svelte';
 	export let name;
 	export let attributes;
 </script>
 
-<a href={'/assets/icons/svgs/' + name + '.svg'} download>
-	<Icon {name} {attributes} />
-	<p>{name}</p>
+<a
+	href={'/assets/icons/svgs/' + name + '.svg'}
+	title="Download {name}"
+	download
+	class="flex flex-col justify-center items-center border border-slate-300 dark:border-slate-600 px-2 py-4 rounded-lg text-slate-600 dark:text-slate-300 w-full hover:bg-slate-100 dark:hover:bg-slate-700"
+>
+	<Pangol {name} {attributes} classes="w-24 h-24 p-2" />
+	<p class="text-xs font-open font-light p-2">{name}</p>
 </a>
