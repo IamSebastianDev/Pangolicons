574f21f59eaccd4d1893243853b8dfb30d0e7a5d
Sebastian <sebastian@iamsebastian.dev>
2021-12-11 20:42:13 +0100

    Renamed Navigationlist to NavList to be in line with other nav components

---
+++ docs/src/components/Navigation/NavList.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Pangol from '../UI/Pangol.svelte';
+	import NavItem from './NavItem.svelte';
+	import NavLink from './NavLink.svelte';
+
+	export let attributes = {};
+</script>
+
+<NavItem>
+	<NavLink href="/docs" title="Documentation">
+		<Pangol name="text" {attributes}>Docs</Pangol>
+	</NavLink>
+</NavItem>
+<NavItem>
+	<NavLink href="/packages" title="Packages">
+		<Pangol name="package" {attributes}>Packages</Pangol>
+	</NavLink>
+</NavItem>
+<NavItem>
+	<NavLink
+		href="https://github.com/iamsebastiandev/pangolicons"
+		title="Github"
+		target="_blank"
+	>
+		<Pangol name="github" {attributes}>Github</Pangol>
+	</NavLink>
+</NavItem>
