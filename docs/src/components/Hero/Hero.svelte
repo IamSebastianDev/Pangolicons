39445fda4e044101cfeebd5eee8a9379d72fe96a
Sebastian <sebastian@iamsebastian.dev>
2021-12-11 22:40:14 +0100

    Recentered headings in Hero

--- docs/src/components/Hero/Hero.svelte
+++ docs/src/components/Hero/Hero.svelte
@@ -4,12 +4,12 @@
 
 <Section id="hero">
 	<h1
-		class="text-5xl md:text-8xl text-center font-light font-comfortaa dark:text-white text-slate-700 py-6"
+		class="text-5xl md:text-8xl text-center font-light font-comfortaa dark:text-white text-slate-700 py-6 mx-auto"
 	>
 		Pangol<span class="text-pangol-500 font-semibold">icons</span>
 	</h1>
 	<h3
-		class="text-base md:text-xl text-center font-open font-thin dark:text-white text-slate-700 py-6 max-w-screen-sm tracking-wider"
+		class="text-base md:text-xl text-center font-open font-thin dark:text-white text-slate-700 py-6 max-w-screen-sm tracking-wider mx-auto"
 	>
 		A modern & minimal open source SVG Icon library. As cute & fierce as
 		it's name suggests.
