1d7cb0dd7941dd0792cbcad7acb3f0ad4529a12a
Sebastian <sebastian@iamsebastian.dev>
2021-12-11 22:44:09 +0100

    Changed hover effect from background to ring

--- docs/src/components/Display/Iconcard.svelte
+++ docs/src/components/Display/Iconcard.svelte
@@ -8,7 +8,7 @@
 	href={'/assets/icons/svgs/' + name + '.svg'}
 	title="Download {name}"
 	download
-	class="flex flex-col justify-center items-center border border-slate-300 dark:border-slate-600 px-2 py-4 rounded-lg text-slate-600 dark:text-slate-300 w-full hover:bg-slate-100 dark:hover:bg-slate-700"
+	class="flex flex-col justify-center items-center border border-slate-300 dark:border-slate-600 px-2 py-4 rounded-lg text-slate-600 dark:text-slate-300 w-full hover:border-pangol-100 hover:ring-2 ring-pangol-100"
 >
 	<Pangol {name} {attributes} classes="w-24 h-24 p-2" />
 	<p class="text-xs font-open font-light p-2">{name}</p>
