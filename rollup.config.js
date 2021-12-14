4fe33e5db9fa39d911fbe13cd41a1bd3b74cf422
Sebastian <sebastian@iamsebastian.dev>
2021-12-14 13:46:54 +0100

    updated rollup config to support bundling a esm & cjs version seperately

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,25 @@
 
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-	input: 'src/pangolicons.mjs',
-	output: [
-		{
-			file: './dist/pangolicons.js',
-			format: 'cjs',
-			plugins: [terser({ module: false, toplevel: true })],
-		},
-		{
-			format: 'esm',
-			file: './dist/pangolicons.esm.mjs',
-			plugins: [terser()],
-		},
-	],
-};
+export default [
+	{
+		input: 'src/pangolicons.mjs',
+		output: [
+			{
+				file: './dist/pangolicons.js',
+				format: 'cjs',
+				plugins: [terser({ module: false, toplevel: true })],
+			},
+		],
+	},
+	{
+		input: 'src/pangolicons.esm.mjs',
+		output: [
+			{
+				format: 'esm',
+				file: './dist/pangolicons.esm.mjs',
+				plugins: [terser()],
+			},
+		],
+	},
+];
