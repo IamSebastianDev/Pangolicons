5dd9c64920c758a79150de45b979e460307c1637
Sebastian <sebastian@iamsebastian.dev>
2021-12-14 13:48:09 +0100

    Added a new test to test for tree-shakeablity of the esm library

---
+++ test/treeshakeable.test.mjs
@@ -0,0 +1,26 @@
+/** @format */
+
+import { exec } from 'child_process';
+
+export default {
+	name: 'Treeshakeable',
+	description: 'Check if the ESM version of the file is treeshakeable',
+	run: async ({ Testrunner }) => {
+		const esmPath = './dist/pangolicons.esm.mjs';
+
+		return new Promise((resolve, reject) => {
+			exec(`npx agadoo ${esmPath}`, (error, stdout, stderr) => {
+				if (error !== null) {
+					reject({ report: error, result: false });
+				}
+
+				if (!stderr.includes('Failed') || stdout.includes('Failed')) {
+					resolve({
+						result: true,
+						report: 'Success! The library is fully tree-shakeable.',
+					});
+				}
+			});
+		});
+	},
+};
