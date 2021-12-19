bf5c78061a4dca0bab742a6492792e53000ee277
Sebastian <sebastian@iamsebastian.dev>
2021-12-19 20:58:59 +0100

    Added a Hero section

---
+++ docs/src/lib/Hero/Hero.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Section from '../UI/Section.svelte';
+	import HeroLinks from './HeroLinks.svelte';
+</script>
+
+<Section id="hero">
+	<h1
+		class="text-5xl md:text-8xl text-center font-light font-comfortaa dark:text-white text-slate-700 py-6 mx-auto"
+	>
+		Pangol<span class="text-pangol-500 font-semibold">icons</span>
+	</h1>
+	<h3
+		class="text-base md:text-xl text-center font-open font-thin dark:text-white text-slate-700 py-6 max-w-screen-sm tracking-wider mx-auto"
+	>
+		A modern & minimal open source SVG Icon library. As cute & fierce as
+		it's name suggests.
+	</h3>
+	<HeroLinks />
+</Section>
