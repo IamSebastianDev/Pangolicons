11ceaab129f4a8e8d2be4147f0495236e449e3b5
Sebastian <sebastian@iamsebastian.dev>
2021-12-19 20:50:54 +0100

    Added dynamic route for packages that will autogenerate a page out of the packages readme

---
+++ docs/src/routes/packages/index.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('/api/getPackages');
+
+		if (res.ok) return { props: { packages: await res.json() } };
+
+		return {
+			status: res.status,
+			error: new Error(),
+		};
+	}
+</script>
+
+<script>
+	import Section from '$lib/UI/Section.svelte';
+	import Package from '../../lib/Packages/Package.svelte';
+
+	export let packages;
+</script>
+
+<svelte:head>
+	<title>Pangolicons | Packages</title>
+</svelte:head>
+
+<Section id="packages">
+	<div class="text-center my-8 text-slate-700 dark:text-white">
+		<h1 class="font-comfortaa font-semibold text-5xl py-4 ">Packages</h1>
+		<p class="font-open text-xl py-4">
+			Below is a (hopefully) evergrowing list of packages available for
+			Pangolicons.
+		</p>
+	</div>
+	<div class="flex flex-col md:grid md:grid-cols-2 gap-24">
+		{#each packages as pkg}
+			<Package {pkg} />
+		{/each}
+	</div>
+</Section>
