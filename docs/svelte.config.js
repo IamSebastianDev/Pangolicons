fad05334010ac71e3e5ea42a456289193e1ee941
Sebastian <sebastian@iamsebastian.dev>
2021-12-19 21:17:38 +0100

    updated svelte config to enable heroku deployment

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -19,12 +19,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
+		adapter: adapter({ env: { port: process.env.PORT } }),
 	},
 };
 
