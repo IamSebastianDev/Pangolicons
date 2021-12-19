f128fb809c2e8aa5ba4e923d644947676041d0eb
Sebastian <sebastian@iamsebastian.dev>
2021-12-19 20:59:53 +0100

    added various link components

---
+++ docs/src/lib/UI/Link.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let href = '';
+	export let target = '_self';
+	export let title = '';
+	export let classes = '';
+</script>
+
+<a
+	class="text-slate-500 dark:text-slate-300 hover:text-slate-800 dark:hover:text-white hover:underline {classes}"
+	{href}
+	{target}
+	{title}
+	rel="norefferer noopener"
+	on:click
+>
+	<slot />
+</a>
---
+++ docs/src/lib/UI/SocialLinks.svelte
@@ -0,0 +1,21 @@
+<script>
+	import NavLink from '../Navigation/NavLink.svelte';
+	import Pangol from './Pangol.svelte';
+</script>
+
+<div
+	class="flex flex-row justify-center items-center absolute bottom-0 m-4 p-2"
+>
+	<NavLink
+		href="https://twitter.com/iamsebastiandev"
+		classes="mx-4 p-2 hover:bg-pangol-300 dark:hover:bg-white hover:text-white dark:hover:text-pangol-500 rounded-sm"
+	>
+		<Pangol name="twitter" />
+	</NavLink>
+	<NavLink
+		href="https://github.com/iamsebastiandev"
+		classes="mx-4 p-2 hover:bg-pangol-300 dark:hover:bg-white hover:text-white dark:hover:text-pangol-500 rounded-sm"
+	>
+		<Pangol name="github" />
+	</NavLink>
+</div>
