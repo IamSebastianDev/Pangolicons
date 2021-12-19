e468b9818e1280cb1728d1d30aa2e3dd808e1f29
Sebastian <sebastian@iamsebastian.dev>
2021-12-19 20:55:19 +0100

    renamed components folder to lib folder as per spec

--- docs/src/components/Navigation/NavItem.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let classes = '';
-</script>
-
-<li class="px-6 py-4 {classes}">
-	<slot />
-</li>
--- docs/src/components/UI/Section.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let id;
-</script>
-
-<section {id} class="py-10 my-10 flex flex-col justify-center">
-	<slot />
-</section>
