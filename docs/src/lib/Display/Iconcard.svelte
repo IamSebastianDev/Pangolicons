ceac948524bcadb46a3639006e3f16f7fe535a41
Sebastian <sebastian@iamsebastian.dev>
2021-12-24 10:45:06 +0100

    Changed download url to dynamic url according to parameters.

--- docs/src/lib/Display/Iconcard.svelte
+++ docs/src/lib/Display/Iconcard.svelte
@@ -8,9 +8,12 @@
 
 <Card classes="hover:border-pangol-100 hover:ring-2 ring-pangol-100 w-full">
 	<a
-		href={'/assets/icons/svgs/' + name + '.svg'}
+		href="/api/icon?name={name}&color={attributes.color.replace(
+			'#',
+			''
+		)}&width={attributes['stroke-width']}&size={attributes.height}"
 		title="Download {name}"
-		download
+		download="{name}.svg"
 		class="flex flex-col justify-center items-center h-full w-full"
 	>
 		<Pangol {name} {attributes} classes="w-24 h-24 p-2" />
