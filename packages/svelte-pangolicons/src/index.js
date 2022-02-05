9992afdf0652de106df81603504b288c883868ab
Sebastian <sebastian@iamsebastian.dev>
2022-02-05 23:55:39 +0100

    FIX: Fixed a typo in the type definitions.

--- packages/svelte-pangolicons/src/index.js
+++ packages/svelte-pangolicons/src/index.js
@@ -1,256 +1,512 @@
 export { default as Airplay } from './icons/Airplay.svelte';
+
 export { default as AlignCenter } from './icons/AlignCenter.svelte';
+
 export { default as AlignJustify } from './icons/AlignJustify.svelte';
+
 export { default as AlignLeft } from './icons/AlignLeft.svelte';
+
 export { default as AlignRight } from './icons/AlignRight.svelte';
+
 export { default as Archive } from './icons/Archive.svelte';
+
 export { default as ArrowDown } from './icons/ArrowDown.svelte';
+
 export { default as ArrowLeft } from './icons/ArrowLeft.svelte';
+
 export { default as ArrowLeftDown } from './icons/ArrowLeftDown.svelte';
+
 export { default as ArrowLeftUp } from './icons/ArrowLeftUp.svelte';
+
 export { default as ArrowRight } from './icons/ArrowRight.svelte';
+
 export { default as ArrowRightDown } from './icons/ArrowRightDown.svelte';
+
 export { default as ArrowRightUp } from './icons/ArrowRightUp.svelte';
+
 export { default as ArrowUp } from './icons/ArrowUp.svelte';
+
 export { default as BarChart } from './icons/BarChart.svelte';
+
 export { default as BarChartAlternative } from './icons/BarChartAlternative.svelte';
+
 export { default as BarChartPositive } from './icons/BarChartPositive.svelte';
+
 export { default as BatteryCharged } from './icons/BatteryCharged.svelte';
+
 export { default as BatteryEmpty } from './icons/BatteryEmpty.svelte';
+
 export { default as Bell } from './icons/Bell.svelte';
+
 export { default as BellOff } from './icons/BellOff.svelte';
+
 export { default as BellRinging } from './icons/BellRinging.svelte';
+
 export { default as Bluetooth } from './icons/Bluetooth.svelte';
+
 export { default as BluetoothConnected } from './icons/BluetoothConnected.svelte';
+
 export { default as BluetoothOff } from './icons/BluetoothOff.svelte';
+
 export { default as BluetoothSearching } from './icons/BluetoothSearching.svelte';
+
 export { default as Bold } from './icons/Bold.svelte';
+
 export { default as BookClosed } from './icons/BookClosed.svelte';
+
 export { default as BookOpen } from './icons/BookOpen.svelte';
+
 export { default as Bookmark } from './icons/Bookmark.svelte';
+
 export { default as Box } from './icons/Box.svelte';
+
 export { default as Calendar } from './icons/Calendar.svelte';
+
 export { default as Camera } from './icons/Camera.svelte';
+
 export { default as CameraOff } from './icons/CameraOff.svelte';
+
 export { default as CastToMedia } from './icons/CastToMedia.svelte';
+
 export { default as Checkmark } from './icons/Checkmark.svelte';
+
 export { default as CheckmarkCircle } from './icons/CheckmarkCircle.svelte';
+
 export { default as ChevronDown } from './icons/ChevronDown.svelte';
+
 export { default as ChevronDownUp } from './icons/ChevronDownUp.svelte';
+
 export { default as ChevronFirst } from './icons/ChevronFirst.svelte';
+
 export { default as ChevronLast } from './icons/ChevronLast.svelte';
+
 export { default as ChevronLeft } from './icons/ChevronLeft.svelte';
+
 export { default as ChevronLeftRight } from './icons/ChevronLeftRight.svelte';
+
 export { default as ChevronRight } from './icons/ChevronRight.svelte';
+
 export { default as ChevronUp } from './icons/ChevronUp.svelte';
+
 export { default as ChevronUpDown } from './icons/ChevronUpDown.svelte';
+
 export { default as ChevronrightLeft } from './icons/ChevronrightLeft.svelte';
+
 export { default as ChevronsDown } from './icons/ChevronsDown.svelte';
+
 export { default as ChevronsLeft } from './icons/ChevronsLeft.svelte';
+
 export { default as ChevronsRight } from './icons/ChevronsRight.svelte';
+
 export { default as ChevronsUp } from './icons/ChevronsUp.svelte';
+
 export { default as Circle } from './icons/Circle.svelte';
+
 export { default as CircleArrowDown } from './icons/CircleArrowDown.svelte';
+
 export { default as CircleArrowLeft } from './icons/CircleArrowLeft.svelte';
+
 export { default as CircleArrowLeftDown } from './icons/CircleArrowLeftDown.svelte';
+
 export { default as CircleArrowLeftUp } from './icons/CircleArrowLeftUp.svelte';
+
 export { default as CircleArrowRight } from './icons/CircleArrowRight.svelte';
+
 export { default as CircleArrowRightDown } from './icons/CircleArrowRightDown.svelte';
+
 export { default as CircleArrowRightUp } from './icons/CircleArrowRightUp.svelte';
+
 export { default as CircleArrowUp } from './icons/CircleArrowUp.svelte';
+
 export { default as CircleMinus } from './icons/CircleMinus.svelte';
+
 export { default as CirclePlus } from './icons/CirclePlus.svelte';
+
 export { default as CircleX } from './icons/CircleX.svelte';
+
 export { default as Clipboard } from './icons/Clipboard.svelte';
+
 export { default as ClipboardNotes } from './icons/ClipboardNotes.svelte';
+
 export { default as Cloud } from './icons/Cloud.svelte';
+
 export { default as CloudDrizzle } from './icons/CloudDrizzle.svelte';
+
 export { default as CloudFog } from './icons/CloudFog.svelte';
+
 export { default as CloudHail } from './icons/CloudHail.svelte';
+
 export { default as CloudLightning } from './icons/CloudLightning.svelte';
+
 export { default as CloudMoon } from './icons/CloudMoon.svelte';
+
 export { default as CloudRain } from './icons/CloudRain.svelte';
+
 export { default as CloudSnow } from './icons/CloudSnow.svelte';
+
 export { default as CloudStorm } from './icons/CloudStorm.svelte';
+
 export { default as CloudSun } from './icons/CloudSun.svelte';
+
 export { default as Clouds } from './icons/Clouds.svelte';
+
 export { default as Code } from './icons/Code.svelte';
+
 export { default as Codepen } from './icons/Codepen.svelte';
+
 export { default as ConvertToLink } from './icons/ConvertToLink.svelte';
+
 export { default as ConvertToOrderedList } from './icons/ConvertToOrderedList.svelte';
+
 export { default as ConvertToUnorderedList } from './icons/ConvertToUnorderedList.svelte';
+
 export { default as ConvertToUnstyledList } from './icons/ConvertToUnstyledList.svelte';
+
 export { default as Cookie } from './icons/Cookie.svelte';
+
 export { default as CornerArrowDownLeft } from './icons/CornerArrowDownLeft.svelte';
+
 export { default as CornerArrowDownRight } from './icons/CornerArrowDownRight.svelte';
+
 export { default as CornerArrowLeftDown } from './icons/CornerArrowLeftDown.svelte';
+
 export { default as CornerArrowLeftUp } from './icons/CornerArrowLeftUp.svelte';
+
 export { default as CornerArrowRightDown } from './icons/CornerArrowRightDown.svelte';
+
 export { default as CornerArrowRightUp } from './icons/CornerArrowRightUp.svelte';
+
 export { default as CornerArrowUpLeft } from './icons/CornerArrowUpLeft.svelte';
+
 export { default as CornerArrowUpRight } from './icons/CornerArrowUpRight.svelte';
+
 export { default as Creditcard } from './icons/Creditcard.svelte';
+
 export { default as Crown } from './icons/Crown.svelte';
+
 export { default as Database } from './icons/Database.svelte';
+
 export { default as Delimitor } from './icons/Delimitor.svelte';
+
 export { default as Display } from './icons/Display.svelte';
+
 export { default as Download } from './icons/Download.svelte';
+
 export { default as DragIndicator } from './icons/DragIndicator.svelte';
+
 export { default as Edit } from './icons/Edit.svelte';
+
 export { default as Envelope } from './icons/Envelope.svelte';
+
 export { default as ExternalLink } from './icons/ExternalLink.svelte';
+
 export { default as EyeClosed } from './icons/EyeClosed.svelte';
+
 export { default as EyeOpen } from './icons/EyeOpen.svelte';
+
 export { default as EyeStriked } from './icons/EyeStriked.svelte';
+
 export { default as Facebook } from './icons/Facebook.svelte';
+
 export { default as Fader } from './icons/Fader.svelte';
+
 export { default as FastForward } from './icons/FastForward.svelte';
+
 export { default as File } from './icons/File.svelte';
+
 export { default as Filter } from './icons/Filter.svelte';
+
 export { default as FnBackspace } from './icons/FnBackspace.svelte';
+
 export { default as FnCaps } from './icons/FnCaps.svelte';
+
 export { default as FnCommand } from './icons/FnCommand.svelte';
+
 export { default as FnControl } from './icons/FnControl.svelte';
+
 export { default as FnEscape } from './icons/FnEscape.svelte';
+
 export { default as FnOption } from './icons/FnOption.svelte';
+
 export { default as FnReturn } from './icons/FnReturn.svelte';
+
 export { default as FnShift } from './icons/FnShift.svelte';
+
 export { default as FnSpacebar } from './icons/FnSpacebar.svelte';
+
 export { default as FnTab } from './icons/FnTab.svelte';
+
 export { default as Folder } from './icons/Folder.svelte';
+
 export { default as Folder2 } from './icons/Folder2.svelte';
+
 export { default as FolderMinus } from './icons/FolderMinus.svelte';
+
 export { default as FolderPlus } from './icons/FolderPlus.svelte';
+
 export { default as Fork } from './icons/Fork.svelte';
+
 export { default as Form } from './icons/Form.svelte';
+
 export { default as Frown } from './icons/Frown.svelte';
+
 export { default as FullscreenEnter } from './icons/FullscreenEnter.svelte';
+
 export { default as FullscreenExit } from './icons/FullscreenExit.svelte';
+
 export { default as Gear } from './icons/Gear.svelte';
+
 export { default as Ghost } from './icons/Ghost.svelte';
+
 export { default as Github } from './icons/Github.svelte';
+
 export { default as Guitar } from './icons/Guitar.svelte';
+
 export { default as Haze } from './icons/Haze.svelte';
+
 export { default as HeadingDecrease } from './icons/HeadingDecrease.svelte';
+
 export { default as HeadingIncrease } from './icons/HeadingIncrease.svelte';
+
 export { default as Heart } from './icons/Heart.svelte';
+
 export { default as Hexagon } from './icons/Hexagon.svelte';
+
 export { default as HorizontalMore } from './icons/HorizontalMore.svelte';
+
 export { default as Icecream } from './icons/Icecream.svelte';
+
 export { default as Image } from './icons/Image.svelte';
+
 export { default as InfoCircle } from './icons/InfoCircle.svelte';
+
 export { default as InfoSquare } from './icons/InfoSquare.svelte';
+
 export { default as Instagram } from './icons/Instagram.svelte';
+
 export { default as Italic } from './icons/Italic.svelte';
+
 export { default as Jellyfish } from './icons/Jellyfish.svelte';
+
 export { default as Key } from './icons/Key.svelte';
+
 export { default as Layers } from './icons/Layers.svelte';
+
 export { default as Layout } from './icons/Layout.svelte';
+
 export { default as LayoutColumns } from './icons/LayoutColumns.svelte';
+
 export { default as LayoutGrid } from './icons/LayoutGrid.svelte';
+
 export { default as LayoutHorizontal } from './icons/LayoutHorizontal.svelte';
+
 export { default as LayoutList } from './icons/LayoutList.svelte';
+
 export { default as LayoutMixed } from './icons/LayoutMixed.svelte';
+
 export { default as LayoutVertical } from './icons/LayoutVertical.svelte';
+
 export { default as Library } from './icons/Library.svelte';
+
 export { default as List } from './icons/List.svelte';
+
 export { default as Location } from './icons/Location.svelte';
+
 export { default as Lock } from './icons/Lock.svelte';
+
 export { default as Login } from './icons/Login.svelte';
+
 export { default as LogoutLeft } from './icons/LogoutLeft.svelte';
+
 export { default as LogoutRight } from './icons/LogoutRight.svelte';
+
 export { default as Marker } from './icons/Marker.svelte';
+
 export { default as Menu } from './icons/Menu.svelte';
+
 export { default as Microphone } from './icons/Microphone.svelte';
+
 export { default as MicrophoneOff } from './icons/MicrophoneOff.svelte';
+
 export { default as Minus } from './icons/Minus.svelte';
+
 export { default as MoonRough } from './icons/MoonRough.svelte';
+
 export { default as MoonStylized } from './icons/MoonStylized.svelte';
+
 export { default as Move } from './icons/Move.svelte';
+
 export { default as MoveDiagonal } from './icons/MoveDiagonal.svelte';
+
 export { default as MoveDiagonalMirrored } from './icons/MoveDiagonalMirrored.svelte';
+
 export { default as MoveHorizontal } from './icons/MoveHorizontal.svelte';
+
 export { default as MoveVertical } from './icons/MoveVertical.svelte';
+
 export { default as Mug } from './icons/Mug.svelte';
+
 export { default as Music } from './icons/Music.svelte';
+
 export { default as Neutral } from './icons/Neutral.svelte';
+
 export { default as Pangolin } from './icons/Pangolin.svelte';
+
 export { default as Parcel } from './icons/Parcel.svelte';
+
 export { default as Pause } from './icons/Pause.svelte';
+
 export { default as Phone } from './icons/Phone.svelte';
+
 export { default as PhoneCall } from './icons/PhoneCall.svelte';
+
 export { default as PhoneIncoming } from './icons/PhoneIncoming.svelte';
+
 export { default as PhoneMissed } from './icons/PhoneMissed.svelte';
+
 export { default as PhoneOff } from './icons/PhoneOff.svelte';
+
 export { default as PhoneOutgoing } from './icons/PhoneOutgoing.svelte';
+
 export { default as PieChart } from './icons/PieChart.svelte';
+
 export { default as Play } from './icons/Play.svelte';
+
 export { default as Plus } from './icons/Plus.svelte';
+
 export { default as Pothos } from './icons/Pothos.svelte';
+
 export { default as Power } from './icons/Power.svelte';
+
 export { default as Rainbow } from './icons/Rainbow.svelte';
+
 export { default as Refresh } from './icons/Refresh.svelte';
+
 export { default as RefreshCCW } from './icons/RefreshCCW.svelte';
+
 export { default as RefreshCW } from './icons/RefreshCW.svelte';
+
 export { default as Repeat } from './icons/Repeat.svelte';
+
 export { default as ResizeImage } from './icons/ResizeImage.svelte';
+
 export { default as Rewind } from './icons/Rewind.svelte';
+
 export { default as Robot } from './icons/Robot.svelte';
+
 export { default as Search } from './icons/Search.svelte';
+
 export { default as Server } from './icons/Server.svelte';
+
 export { default as Settings } from './icons/Settings.svelte';
+
 export { default as Shield } from './icons/Shield.svelte';
+
 export { default as Shuffle } from './icons/Shuffle.svelte';
+
 export { default as Sidebar } from './icons/Sidebar.svelte';
+
 export { default as SkipBack } from './icons/SkipBack.svelte';
+
 export { default as SkipForward } from './icons/SkipForward.svelte';
+
 export { default as Sliders } from './icons/Sliders.svelte';
+
 export { default as Smartphone } from './icons/Smartphone.svelte';
+
 export { default as Smile } from './icons/Smile.svelte';
+
 export { default as Snail } from './icons/Snail.svelte';
+
 export { default as Snowflake } from './icons/Snowflake.svelte';
+
 export { default as Snpachat } from './icons/Snpachat.svelte';
+
 export { default as Spoon } from './icons/Spoon.svelte';
+
 export { default as Square } from './icons/Square.svelte';
+
 export { default as SquareMinus } from './icons/SquareMinus.svelte';
+
 export { default as SquarePlus } from './icons/SquarePlus.svelte';
+
 export { default as SquareX } from './icons/SquareX.svelte';
+
 export { default as Star } from './icons/Star.svelte';
+
 export { default as StarHalf } from './icons/StarHalf.svelte';
+
 export { default as Strikethrough } from './icons/Strikethrough.svelte';
+
 export { default as SunBig } from './icons/SunBig.svelte';
+
 export { default as SunSmall } from './icons/SunSmall.svelte';
+
 export { default as Sword } from './icons/Sword.svelte';
+
 export { default as Tablet } from './icons/Tablet.svelte';
+
 export { default as Terminal } from './icons/Terminal.svelte';
+
 export { default as Text } from './icons/Text.svelte';
+
 export { default as Title } from './icons/Title.svelte';
+
 export { default as TrashcanEmpty } from './icons/TrashcanEmpty.svelte';
+
 export { default as TrashcanFilled } from './icons/TrashcanFilled.svelte';
+
 export { default as Trello } from './icons/Trello.svelte';
+
 export { default as Triangle } from './icons/Triangle.svelte';
+
 export { default as TriangleInverted } from './icons/TriangleInverted.svelte';
+
 export { default as Twitch } from './icons/Twitch.svelte';
+
 export { default as Twitter } from './icons/Twitter.svelte';
+
 export { default as Umbrella } from './icons/Umbrella.svelte';
+
 export { default as Underline } from './icons/Underline.svelte';
+
 export { default as Unlock } from './icons/Unlock.svelte';
+
 export { default as User } from './icons/User.svelte';
+
 export { default as UserAdd } from './icons/UserAdd.svelte';
+
 export { default as UserMultiple } from './icons/UserMultiple.svelte';
+
 export { default as UserRemove } from './icons/UserRemove.svelte';
+
 export { default as VerticalMore } from './icons/VerticalMore.svelte';
+
 export { default as Voicemail } from './icons/Voicemail.svelte';
+
 export { default as VolumeHigh } from './icons/VolumeHigh.svelte';
+
 export { default as VolumeLow } from './icons/VolumeLow.svelte';
+
 export { default as VolumeMedium } from './icons/VolumeMedium.svelte';
+
 export { default as VolumeMute } from './icons/VolumeMute.svelte';
+
 export { default as Webcam } from './icons/Webcam.svelte';
+
 export { default as Wifi } from './icons/Wifi.svelte';
+
 export { default as WifiOff } from './icons/WifiOff.svelte';
+
 export { default as Wind } from './icons/Wind.svelte';
+
 export { default as Wrench } from './icons/Wrench.svelte';
+
 export { default as X } from './icons/X.svelte';
+
 export { default as Youtube } from './icons/Youtube.svelte';
+
 export { default as ZoomIn } from './icons/ZoomIn.svelte';
+
 export { default as ZoomOut } from './icons/ZoomOut.svelte';
+
