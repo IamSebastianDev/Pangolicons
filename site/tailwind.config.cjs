13328cbcfe6c7789a53c8b0608a28554f93d4cee
Sebastian <sebastian@iamsebastian.dev>
2022-03-03 13:32:39 +0100

    FEAT: Added fonts and colour extension to tailwind

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -1,13 +1,29 @@
+/** @format */
+
 const typography = require('@tailwindcss/typography');
 
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkmode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				comfortaa: ['Comfortaa', 'sans-serif'],
+				open: ['Open sans', 'sans-serif'],
+			},
+			colors: {
+				pangol: {
+					100: '#8CB6CF',
+					300: '#619BBD',
+					500: '#407899',
+					700: '#305B73',
+					900: '#1E3948',
+				},
+			},
+		},
 	},
 
-	plugins: [typography]
+	plugins: [typography],
 };
 
 module.exports = config;
