7b1183dfb3ab6d62b88b661869875680f90f00a2
Sebastian <sebastian@iamsebastian.dev>
2022-03-05 23:07:53 +0100

    PROG: Added custom colors and fonts to tailwind config

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -3,27 +3,27 @@
 const typography = require('@tailwindcss/typography');
 
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkmode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				comfortaa: ['Comfortaa', 'sans-serif'],
-				open: ['Open sans', 'sans-serif'],
-			},
-			colors: {
-				pangol: {
-					100: '#8CB6CF',
-					300: '#619BBD',
-					500: '#407899',
-					700: '#305B73',
-					900: '#1E3948',
-				},
-			},
-		},
-	},
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                comfortaa: ['Comfortaa', 'sans-serif'],
+                open: ['Open sans', 'sans-serif'],
+            },
+            colors: {
+                pangol: {
+                    100: '#8CB6CF',
+                    300: '#619BBD',
+                    500: '#407899',
+                    700: '#305B73',
+                    900: '#1E3948',
+                },
+            },
+        },
+    },
 
-	plugins: [typography],
+    plugins: [typography],
 };
 
 module.exports = config;
