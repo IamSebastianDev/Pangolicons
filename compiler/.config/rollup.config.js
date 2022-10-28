57cf8225061006029ac00004e09e65a6d7610c6c
Sebastian <sebastian@iamsebastian.dev>
2022-10-28 13:16:30 +0200

    chore: create rollup config to build compiler

--- compiler/.config/rollup.config.js
+++ compiler/.config/rollup.config.js
@@ -1,3 +1,29 @@
 /** @format */
 
-export default [{}];
+import esbuild from 'rollup-plugin-esbuild';
+import cleanup from 'rollup-plugin-cleanup';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+
+export default [
+    {
+        input: './compiler/src/index.ts',
+        output: [
+            {
+                file: './compiler/bin/index.js',
+                format: 'es',
+            },
+        ],
+        external: (id) => !/^[./]/.test(id),
+        plugins: [
+            resolve({ preferBuiltins: true }),
+            commonjs(),
+            json(),
+            esbuild({
+                tsconfig: '../tsconfig.json',
+            }),
+            cleanup({ extensions: ['.ts'] }),
+        ],
+    },
+];
